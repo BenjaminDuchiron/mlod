f805ad441a410e424d6b875a32365b02f1ab7e64
Ben <benjamin.duchiron@etu.imt-lille-douai.fr>
2021-10-07 16:19:51 +0000

    test

--- workspace/hello.c
+++ workspace/hello.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
-int a = 2;
+float prix_billet_avion(int age, float prix_plein_tarif) {
+    if (age<=2)
+        prix_plein_tarif= 0.1*prix_plein_tarif ;
+    else {
+        if(age<=12)
+            prix_plein_tarif=0.5*prix_plein_tarif;
+        
+    }
+    return prix_plein_tarif;
+}
 
-int main(void) {
-    int a = 3;
-
-    printf("hello");
-
-    a = 4;
+int main(void){
+    int age =11;
+    float prix_plein_tarif = 100.0;
+    printf ("prix_billet_avion= %f ",prix_billet_avion(age,plein));
 
     return 0;
-}
+} 
